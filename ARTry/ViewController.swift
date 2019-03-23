24a96263083be524b1b3563469f7711c72ac7ab1
Liguo Jiao <liguo@jiao.co.nz>
2019-03-23 22:09:28 +1300

    Demo Finished

--- ARTry/ViewController.swift
+++ ARTry/ViewController.swift
@@ -35,7 +35,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
+        // Object Detection
+        configuration.detectionObjects = ARReferenceObject.referenceObjects(inGroupNamed: "AR_Resources", bundle: Bundle.main)!
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -49,14 +52,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
+        
+        if let objectAnchor = anchor as? ARObjectAnchor {
+            let plane = SCNPlane(width: CGFloat(objectAnchor.referenceObject.extent.x * 0.8), height: CGFloat(objectAnchor.referenceObject.extent.y * 0.5))
+            
+            plane.cornerRadius = plane.width / 8
+            
+            let spriteKitScene = SKScene(fileNamed: "ProductInfo")
+            
+            plane.firstMaterial?.diffuse.contents = spriteKitScene
+            plane.firstMaterial?.isDoubleSided = true
+            plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+            
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.position = SCNVector3Make(objectAnchor.referenceObject.center.x, objectAnchor.referenceObject.center.y, objectAnchor.referenceObject.center.z)
+            
+            node.addChildNode(planeNode)
+            print("reconized success")
+        }
+        
         return node
     }
-*/
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
